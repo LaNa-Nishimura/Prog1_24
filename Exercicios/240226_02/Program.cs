136ad1b509acf2a64eedf885816925d6b71f9615
Allana Vitória Zucco Paganini <235199@unoesc.lan>
2024-02-26 21:52:59 -0300

    Aula 26-02-24

---
+++ Exercicios/240226_02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+string nome = string.Empty;
+Console.WriteLine("Qual é o seu nome?");
+nome = Console.ReadLine();
+
+string sobrenome = string.Empty;
+Console.WriteLine("Qual é o seu sobrenome?");
+sobrenome = Console.ReadLine();
+
+string email = string.Empty;
+Console.WriteLine("Qual é o seu email?");
+email = Console.ReadLine();
+
+string datan = string.Empty;
+Console.WriteLine("Qual é a sua data de mascimento?");
+datan = Console.ReadLine();
+
+string sexo = string.Empty;
+Console.WriteLine("Qual é o seu sexo?");
+sexo = Console.ReadLine();
+
+string cep = string.Empty;
+Console.WriteLine("Qual é o seu CEP?");
+cep = Console.ReadLine();
+
+string estp = string.Empty;
+Console.WriteLine("Em que país e estado você mora?");
+estp = Console.ReadLine();
+
+string end = string.Empty;
+Console.WriteLine("Qual é o seu endereço? (Rua, N°, Bairro)");
+end = Console.ReadLine();
+
+string listagem = $" Ficha de {nome} :D \n" +
+                  $"Nome Completo: {nome} {sobrenome} \n" +
+                  $"Endereço de E-mail: {email} \n" +
+                  $"Data de Nascimento: {datan} \n" +
+                  $"Sexo: {sexo} \n" +
+                  $"CEP: {cep} \n" +
+                  $"Endereço: {end}, {estp}";
+
+Console.WriteLine(listagem);
