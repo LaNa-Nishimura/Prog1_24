8dbc1c0f3f3b26fac1946034b014f5e87fe37b8c
Allana Vitória Zucco Paganini <235199@unoesc.lan>
2024-03-04 20:22:51 -0300

    Comparando Strings

--- Exercicios/240304_01/Program.cs
+++ Exercicios/240304_01/Program.cs
@@ -1,10 +1,13 @@
-﻿// olá
-//Exercício:
-//1. Fazer checkout em seu repositório local para branch main.
-//2. Baixar as atualizações do repositório remoto em main para o repo local. (pull)
-//(já tinha feito, quando for fazer de novo vai aparecer outra mensagem)
-//3. Crie uma nova branch a partir de main para a aula de hoje, seguindo o padrão de nomenclatura.
-//4. Inclua um comentário no código para ele sofrer uma pequena alteração.
-//5. Adicione essa alteração ao rastreio do git.
-//6. Crie um checkpoint.
-//7. Envie a nova branch local para o repositório remoto.
+﻿//Substituíndo conteúdo de strings
+string nomeCompleto = "Lana Nishimura";
+nomeCompleto = nomeCompleto.Replace("Nishimura", "Donfort");
+
+Console.WriteLine(nomeCompleto);
+
+//Comparação de strings
+bool isNomeEqual = (nomeCompleto == "Lana Nishimura");
+
+bool isNomeEqual2 = string.Equals(nomeCompleto, "Lana Donfort");
+
+Console.WriteLine($"Primeiro resultado: {isNomeEqual}");
+Console.WriteLine($"Segundo resultado: {isNomeEqual2}");
