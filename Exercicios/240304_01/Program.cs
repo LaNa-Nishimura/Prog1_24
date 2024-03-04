992727aa43c56521d85415c78aab8c3754074f04
Allana Vitória Zucco Paganini <235199@unoesc.lan>
2024-03-04 19:36:43 -0300

    Aula 04-03-24

---
+++ Exercicios/240304_01/Program.cs
@@ -0,0 +1,10 @@
+﻿// olá
+//Exercício:
+//1. Fazer checkout em seu repositório local para branch main.
+//2. Baixar as atualizações do repositório remoto em main para o repo local. (pull)
+//(já tinha feito, quando for fazer de novo vai aparecer outra mensagem)
+//3. Crie uma nova branch a partir de main para a aula de hoje, seguindo o padrão de nomenclatura.
+//4. Inclua um comentário no código para ele sofrer uma pequena alteração.
+//5. Adicione essa alteração ao rastreio do git.
+//6. Crie um checkpoint.
+//7. Envie a nova branch local para o repositório remoto.
