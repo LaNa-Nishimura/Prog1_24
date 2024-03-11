00de05119ab448eb9046c1f126032538f816aa2c
Allana Vitória Zucco Paganini <235199@unoesc.lan>
2024-03-11 19:09:47 -0300

    aula04

--- Atividades/Atividade_240304/Program.cs
+++ Atividades/Atividade_240304/Program.cs
@@ -1,38 +1,38 @@
 ﻿Console.Write("Digite o primeiro número: ");
-num1 = Convert.ToDouble(Console.ReadLine());
+double num1 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Qual operação deseja realizar? \n";
-                  "1 - Adição \n";
-                  "2 - Subtração \n";
-                  "3 - Multiplicação \n";
+Console.WriteLine("Qual operação deseja realizar? \n" +
+                  "1 - Adição \n" +
+                  "2 - Subtração \n" +
+                  "3 - Multiplicação \n" +
                   "4 - Divisão");
-operador = Console.ReadLine();
+string operador = Console.ReadLine();
 
 Console.Write("Digite o segundo número: ");
-num2 = Convert.ToDouble(Console.ReadLine());
+double num2 = Convert.ToDouble(Console.ReadLine());
 
 double resultado = 0;
-if (operador == '1') {
+if (operador == "1") {
     resultado = num1 + num2;
+}
+else if (operador == "2") {
+    resultado = num1 - num2;
+}
+else if (operador == "3") {
+    resultado = num1 * num2;
+}
+else if (operador == "4") {
+    if (num2 != 0) {
+        resultado = num1 / num2;
     }
-    else if (operador == '2') {
-        resultado = num1 - num2;
-        }
-        else if (operador == '3') {
-            resultado = num1 * num2;
-            }
-        else if (operador == '4') {
-            if (num2 != 0) {
-                resultado = num1 / num2;
-                }
-            else {
-                Console.WriteLine("Erro: Divisão por zero!");
-                return;
-            }
-        }
-        else {
-            Console.WriteLine("Operador inválido!");
-            return;
-        }
+    else {
+        Console.WriteLine("Erro: Divisão por zero.");
+        return;
+    }
+}
+else {
+    Console.WriteLine("Operador inválido.");
+    return;
+}
 
-        Console.WriteLine("O resultado é: " + resultado);
+Console.WriteLine("O resultado é: " + resultado);
