b78c02fc06f627c67438d775b1f9edf29f723e31
Allana Paganini <allanapaganini11@gmail.com>
2024-03-10 21:38:14 -0300

    Atividade_240304

--- Atividades/Atividade_240304/Program.cs
+++ Atividades/Atividade_240304/Program.cs
@@ -1,13 +1,38 @@
-﻿Console.WriteLine("Digite um número: ");
-primeiroNumero = Console.ReadLine;
+﻿Console.Write("Digite o primeiro número: ");
+num1 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Qual operação deseja realizar? \n";
                   "1 - Adição \n";
                   "2 - Subtração \n";
                   "3 - Multiplicação \n";
                   "4 - Divisão");
-operacao = Console.ReadLine();
+operador = Console.ReadLine();
 
-Console.WriteLine("Digite um número: ");
-segundoNumero = Console.ReadLine;
+Console.Write("Digite o segundo número: ");
+num2 = Convert.ToDouble(Console.ReadLine());
 
+double resultado = 0;
+if (operador == '1') {
+    resultado = num1 + num2;
+    }
+    else if (operador == '2') {
+        resultado = num1 - num2;
+        }
+        else if (operador == '3') {
+            resultado = num1 * num2;
+            }
+        else if (operador == '4') {
+            if (num2 != 0) {
+                resultado = num1 / num2;
+                }
+            else {
+                Console.WriteLine("Erro: Divisão por zero!");
+                return;
+            }
+        }
+        else {
+            Console.WriteLine("Operador inválido!");
+            return;
+        }
+
+        Console.WriteLine("O resultado é: " + resultado);
