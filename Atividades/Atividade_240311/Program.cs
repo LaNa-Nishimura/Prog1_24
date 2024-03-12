05b52aed864842094ff157b9d3f3451c4f48dd8e
Allana Vitória Zucco Paganini <235199@unoesc.lan>
2024-03-11 21:50:41 -0300

    Atividade_240311

---
+++ Atividades/Atividade_240311/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+Console.WriteLine("De qual número será a tabuada: ");
+double tabuada = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("De qual operação será a tabuada: \n" +
+                  "1 - Adição \n" +
+                  "2 - Subtração \n" +
+                  "3 - Multiplicação \n" +
+                  "4 - Divisão");
+string operador = Console.ReadLine();
+
+double resultado = 0;
+
+WriteLine();
+WriteLine($"Tabuada de {tabuada}");
+
+for (int i = 1; i <= 9; i++) {
+    if (operador == "1") {
+        resultado = tabuada + i;
+        WriteLine($"{tabuada} + {i} = {resultado}");
+    }
+
+    else if (operador == "2") {
+        resultado = tabuada - i;
+        WriteLine($"{tabuada} - {i} = {resultado}");
+    }
+    
+    else if (operador == "3") {
+        resultado = tabuada * i;
+        WriteLine($"{tabuada} * {i} = {resultado}");
+    }
+
+    else if (operador == "4") {
+        resultado = tabuada / i;
+        WriteLine($"{tabuada} / {i} = {resultado}");
+    }
+
+    else {
+        WriteLine("Opção Inválida.");
+    }
+}
