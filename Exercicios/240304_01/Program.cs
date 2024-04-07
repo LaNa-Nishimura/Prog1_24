e0e6dd4636035723415aa419885967feab7073d7
LaNa <143635603+LaNa-Nishimura@users.noreply.github.com>
2024-04-07 14:26:48 -0300

    Update Program.cs

--- Exercicios/240304_01/Program.cs
+++ Exercicios/240304_01/Program.cs
@@ -17,7 +17,7 @@ Console.WriteLine($"Segundo resultado: {isNomeEqual2}");
     sbyte: armazena valores entre -128 e 127
     short: armazena valores entre -32,768 e 32,767
     int: armazena valores entre -2,147,483,648 e 2,147,483,647
-    long: armazena valores entre -9,223,372,036,854,775,808 e -9,223,372,036,854,775,807
+    long: armazena valores entre -9,223,372,036,854,775,808 e 9,223,372,036,854,775,807
 */
 
 float myFloat = 10f;
